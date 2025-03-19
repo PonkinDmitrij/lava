62d061f627d2cfcb8c93b6acdacf2fe07da418f0
Dmitrij Ponkin <ponkindmitrij@gmail.com>
2025-03-19 13:19:34 +0300

    feat: add page-header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 import '../scss/style.scss';
+
+const burger = document.querySelector('.burger');
+const navList = document.querySelector('.page-header__nav-list');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger--close');
+  if (burger.classList.contains('burger--close')) {
+    navList.style.maxHeight = `${navList.scrollHeight}px`;
+  } else {
+    navList.style.maxHeight = '';
+  }
+});
